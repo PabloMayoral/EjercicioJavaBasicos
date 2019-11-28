4445c155476f3563384ebcaa32bfa7446f885725
pmart <pmart@LAPTOP-USLI8H8E>
2019-11-28 17:44:46 +0100

    Con el segundo ejercicio terminado

--- src/ejerciciosbasicosjava/EjerciciosBasicosJava.java
+++ src/ejerciciosbasicosjava/EjerciciosBasicosJava.java
@@ -18,7 +18,17 @@ public class EjerciciosBasicosJava {
     }
    
     public int multa(int velocidad, boolean birthday){
-      return 0;
+        if(birthday){//si es mi cumple el policeman me quita 5 de velocidad
+        velocidad+=5;
+        }
+        if(velocidad <=60){
+        return 0;
+        }
+         if(velocidad >60 && velocidad <=80){
+        return 1;
+        }
+         
+      return 2;
     }
     
     /**
@@ -30,7 +40,8 @@ public class EjerciciosBasicosJava {
               System.out.println(ejercicios.fiestaAardillas(50, false));
        System.out.print(ejercicios.fiestaAardillas(70, true));
        System.out.print(ejercicios.multa(60,false));
-
+ System.out.print(ejercicios.multa(65,false));
+  System.out.print(ejercicios.multa(65,true));
     }
     
 }
